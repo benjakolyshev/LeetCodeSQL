a6b1179e7a41c735b8e8c07eea777ce22df8006d
benjakolyshev <57508856+benjakolyshev@users.noreply.github.com>
2022-04-04 01:49:15 -0400

    Time: 1200 ms (5.04%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,3 +1,4 @@
+/*
 SELECT  e.employee_id,
         e.department_id
 FROM (
@@ -7,3 +8,15 @@ FROM (
             RANK() OVER (partition by employee_id ORDER BY       primary_flag ASC) AS rnk
     FROM    Employee ) e
 WHERE e.rnk = 1
+*/
+SELECT  employee_id,
+        department_id
+FROM    Employee
+WHERE   primary_flag = 'Y'
+
+UNION
+
+SELECT  employee_id,
+        department_id
+FROM    Employee
+GROUP BY    employee_id HAVING COUNT(primary_flag) = 1
