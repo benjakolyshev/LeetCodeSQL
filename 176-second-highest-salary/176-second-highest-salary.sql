400981370c38f9223afe3db699c6610f2b55d056
benjakolyshev <57508856+benjakolyshev@users.noreply.github.com>
2022-04-04 11:19:24 -0400

    Time: 185 ms (81.83%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,4 +1,23 @@
-SELECT  MAX(salary) AS SecondHighestSalary
+SELECT  (CASE WHEN (SELECT COUNT(DISTINCT(salary) > 1) FROM Employee) THEN
+(SELECT e1.salary 
+FROM    (
+
+    SELECT  salary
+    FROM    Employee
+    GROUP BY    salary
+    ORDER BY    salary  DESC
+    LIMIT   2   ) e1
+
+LEFT JOIN   (
+
+    SELECT  salary
+    FROM    Employee
+    GROUP BY    salary
+    ORDER BY    salary  DESC
+    LIMIT   1   ) e2
+
+ON  e1.salary = e2.salary
+WHERE   e2.salary IS NULL)
+ELSE NULL END ) SecondHighestSalary
 FROM    Employee
-WHERE   salary < (SELECT    MAX(salary)
-                    FROM    Employee)
+LIMIT   1
