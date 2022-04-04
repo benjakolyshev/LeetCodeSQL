d1cafacd4dc5af6a462ff2c131b98daccada19a0
benjakolyshev <57508856+benjakolyshev@users.noreply.github.com>
2022-04-03 23:24:38 -0400

    Delete 1142-user-activity-for-the-past-30-days-ii directory

--- 1142-user-activity-for-the-past-30-days-ii/1142-user-activity-for-the-past-30-days-ii.sql
+++
@@ -1,3 +0,0 @@
-SELECT  ROUND(IFNULL(COUNT(DISTINCT(session_id))/COUNT(DISTINCT(user_id)),0),2) AS average_sessions_per_user
-FROM    Activity
-WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
