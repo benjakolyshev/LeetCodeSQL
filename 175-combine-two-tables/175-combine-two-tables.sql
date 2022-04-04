dc6914e381d29dccdcacb09680f8f9c704b053e0
benjakolyshev <57508856+benjakolyshev@users.noreply.github.com>
2022-04-04 11:09:57 -0400

    Time: 817 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -1,12 +1,15 @@
-# Write your MySQL query statement below
-SELECT  p.firstName,
-        p.lastName,
+# We do a left join here because we want all of the person's first and last names
+# but we do not care for their addresses if we do not have their names
+SELECT  p.lastName,
+        p.firstName,
         a.city,
         a.state
-FROM    (
-    SELECT  *
-    FROM    Person ) p
-LEFT JOIN   (
-    SELECT  *
-    FROM    Address ) a
-ON p.personId = a.personId
+FROM    
+(
+SELECT  *
+FROM    Person ) p
+LEFT JOIN 
+(
+SELECT  *
+FROM    Address ) a
+ON  p.personId = a.personId
