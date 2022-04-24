0b93a3199ec06b5119f7af57c330205625197b75
benjakolyshev <57508856+benjakolyshev@users.noreply.github.com>
2022-04-24 16:55:42 -0400

    Time: 452 ms (29.67%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -1,15 +1,11 @@
-# We do a left join here because we want all of the person's first and last names
-# but we do not care for their addresses if we do not have their names
-SELECT  p.lastName,
-        p.firstName,
+SELECT  p.firstName,
+        p.lastName,
         a.city,
         a.state
-FROM    
-(
-SELECT  *
-FROM    Person ) p
-LEFT JOIN 
-(
-SELECT  *
-FROM    Address ) a
+FROM    (
+    SELECT  *
+    FROM    Person ) p
+    LEFT JOIN   (
+    SELECT  *
+    FROM    Address ) a
 ON  p.personId = a.personId
