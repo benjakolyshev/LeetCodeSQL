21bfb56a07f3b1fc9f1b3bdf9e0bdc8b23b2ea30
benjakolyshev <57508856+benjakolyshev@users.noreply.github.com>
2022-04-24 21:33:57 -0400

    Time: 288 ms (20.75%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,7 +1,6 @@
-SELECT  MAX(salary) as SecondHighestSalary
-FROM
-(
-SELECT  salary, 
-        DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
-FROM    Employee ) e
-WHERE   e.rnk = 2
+SELECT  MAX(s.salary) as SecondHighestSalary
+FROM    (
+    Select  salary,
+            DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+    FROM    Employee ) s
+WHERE   rnk = 2
